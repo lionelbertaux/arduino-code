02b2b329583f178d922d060bac0454ac3f36305f
lionel <lionel.bertaux@gmail.com>
2018-12-10 22:57:10 +0100

    Add integrated LED Blink for ON/OFF

---
+++ tools/init-serial.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo chmod a+rw /dev//ttyACM0
+sudo stty -F /dev/ttyACM0 cs8 115200 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
